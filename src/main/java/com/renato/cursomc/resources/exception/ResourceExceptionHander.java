7e5de1897016006d169ba97316995276dd7b8263
Renato Leme <renatol1857@gmail.com>
2021-11-04 06:27:02 -0300

    tratamendo 404 - Categoria na encontrada

---
+++ src/main/java/com/renato/cursomc/resources/exception/ResourceExceptionHander.java
@@ -0,0 +1,21 @@
+package com.renato.cursomc.resources.exception;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.renato.cursomc.services.exceptions.ObjectNotFoundException;
+
+@ControllerAdvice
+public class ResourceExceptionHander {
+	
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request){
+		StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
+		
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+	}
+}
